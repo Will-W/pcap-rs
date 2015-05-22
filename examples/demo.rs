1a6372fe1942862c7523d6d444e120e6857d5b14
Will Wykeham <will@wykeham.net>
2015-05-22 15:33:33 +0100

    Function to lookup device name

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,4 +2,10 @@ extern crate pcap;
 
 fn main() {
     println!("Version: '{}'", pcap::lib_version());
+
+    match pcap::lookupdev() {
+        Ok(x) => println!("Device found: {}", x),
+        Err(x) => println!("Lookupdev failed: {}", x),
+    };
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use std::str;
 mod binding;
 use binding::*;
 
+const ERRBUFF_SIZE: usize = 256;
+
 #[test]
 fn it_works() {
 }
@@ -13,4 +15,19 @@ fn it_works() {
 pub fn lib_version() -> &'static str {
     let slice = unsafe { ffi::CStr::from_ptr(pcap_lib_version()) };
     str::from_utf8(slice.to_bytes()).unwrap()
+}
+
+pub fn lookupdev() -> Result<String, String> {
+    let mut errbuff = [0 as libc::c_char; ERRBUFF_SIZE];
+    unsafe {
+        let dev = pcap_lookupdev(errbuff.as_mut_ptr());
+        if dev.is_null() {
+            let slice = ffi::CStr::from_ptr(errbuff.as_ptr());
+            Err(str::from_utf8(slice.to_bytes()).unwrap().to_string())
+        }
+        else {
+            let slice = ffi::CStr::from_ptr(dev);
+            Ok(str::from_utf8(slice.to_bytes()).unwrap().to_string())
+        }
+    }
 }
