c5e17eeb8e1fe4e7484fceb9b769fe3c817abd32
Will Wykeham <will@wykeham.net>
2015-05-22 17:06:20 +0100

    Open and close a session

--- examples/demo.rs
+++ examples/demo.rs
@@ -4,8 +4,15 @@ fn main() {
     println!("Version: '{}'", pcap::lib_version());
 
     match pcap::lookupdev() {
-        Ok(x) => println!("Device found: {}", x),
+        Ok(x) => println!("Device found: '{}'", x),
         Err(x) => println!("Lookupdev failed: {}", x),
     };
+
+    match pcap::Session::open_live("eth0") {
+        Err(x) => println!("Error: **{}**", x),
+        Ok(_) => println!("Session opened Ok"),
+    };
+
+    // pcap::Session::open_live("eth0").unwrap();
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,37 @@ pub fn lookupdev() -> Result<String, String> {
         }
     }
 }
+
+pub struct Session {
+    handle: *mut pcap_t,
+}
+
+impl Session {
+    pub fn open_live(dev: &str) -> Result<Session, String> {
+        let mut errbuff = [0 as libc::c_uchar; ERRBUFF_SIZE];
+        let handle = unsafe {
+             pcap_open_live(ffi::CString::new(dev).unwrap().as_ptr(),
+                            libc::consts::os::c95::BUFSIZ as i32,
+                            1,
+                            1000,
+                            errbuff.as_mut_ptr() as *mut i8
+                            )
+        };
+
+        if handle.is_null() {
+            let slice = str::from_utf8(&errbuff[..]).unwrap();
+            Err(slice.to_owned())
+        }
+        else {
+            Ok( Session { handle: handle } )
+        }
+    }
+}
+
+impl Drop for Session {
+    fn drop(&mut self) {
+        unsafe {
+            pcap_close(self.handle);
+        }
+    }
+}
